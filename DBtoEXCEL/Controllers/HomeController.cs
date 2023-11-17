b947bff53bda5547ecf9f2d2612191a43e88dd16
CIPL1309 <CIPL1309@CIPL1309_DOTNET>
2023-11-17 14:30:51 +0530

    Add project files.

---
+++ DBtoEXCEL/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using ClosedXML.Excel;
+using DBtoEXCEL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace DBtoEXCEL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DataSet ds = this.GetCustomers();
+            return View(ds);
+        }
+
+        [HttpPost]
+        public IActionResult Export()
+        {
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                DataTable dt = this.GetCustomers().Tables[0];
+                wb.Worksheets.Add(dt);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    wb.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Export.xlsx");
+                }
+            }
+        }
+
+        private DataSet GetCustomers()
+        {
+            DataSet ds = new DataSet();
+            string constring = "server=CIPL1309_DOTNET\\MSSQLSERVER19;database=ExcelFileUpload;trusted_connection=true;";
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                string query = "SELECT * FROM Employee";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        sda.Fill(ds);
+                    }
+                }
+            }
+            return ds;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
